6aff243f30da91800caac2ce729a6cf046ce3d65
Mike <mi.su@hispeed.ch>
2021-10-04 22:09:28 +0200

    Storing and retrieving data

--- ipfs.py
+++ ipfs.py
@@ -1,4 +1,5 @@
 import ipfshttpclient
+import codecs
 
 # prerequisites: a running instance of an IPFS daemon
 
@@ -9,6 +10,16 @@ if __name__ == '__main__':
 	# connecting to the local IPFS node and retrieving the client object
 	client = ipfshttpclient.connect()
 	print("IPFS daemon running on version: " + client.version()["Version"])
+
+	# adding a file to IPFS
+	retrieval_data = client.add("data/mytextfile.txt", wrap_with_directory=True)
+	print(retrieval_data)
+
+	# read the file's content via directory hash
+	print((client.cat(retrieval_data[1]["Hash"] + "/" + retrieval_data[0]["Name"])).decode('UTF-16'))
+
+
+
 
 
 
