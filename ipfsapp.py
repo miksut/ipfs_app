ee7a3635f659fe6a363380020f92ae3ac50838ef
Mike <mi.su@hispeed.ch>
2021-10-13 02:27:21 +0200

    Implemented functionality: download, serialise, upload, distribute

--- ipfsapp.py
+++ ipfsapp.py
@@ -1,50 +1,154 @@
+import codecs
 import ipfshttpclient
-import numpy as np
 import json
+import numpy as np
 import os
+import paramiko
+import re
 import requests
 import time
 
 
-class DownloadManagerHTTP:
-	def __init__(self, sourcePaths, downloadDir):
-		self.__sourcePaths = self.__loadSourcePaths(sourcePaths)
-		self.__downloadDir = downloadDir
+class Action:
+	def __init__(self, sourcePaths):
+		self.__sourcePaths = sourcePaths
 		self.__timer = Timer()
 
-	def __loadSourcePaths(self, sourcePaths):
-		with open(sourcePaths, "r") as file_in:
-			paths = json.loads(file_in.read())
-			key = list(paths.keys())[0]
-			return paths[key]
-
-	def downloadFiles(self, runs=1):
-		downloadTimes = np.zeros((len(self.__sourcePaths), runs))
-		print(downloadTimes)
+	def execute(self, fileRemover, runs=1):
+		performanceMeasures = np.zeros((len(self.__sourcePaths), runs))
+		fileIDs = ["" for i in range(len(self.__sourcePaths))]
 
 		for i in range(runs):
 
-			for file in os.scandir(self.__downloadDir):
-				os.remove(file.path)
+			fileRemover.execute()
 
-			for j, url in enumerate(self.__sourcePaths):
+			for j, path in enumerate(self.__sourcePaths):
 				self.__timer.start()
-				response = requests.get(url, allow_redirects=True)
-				with open(self.__downloadDir + "file" + str(j+1) + ".txt", "wb") as file_out:
-					file_out.write(response.content)
-				downloadTimes[j][i] = self.__timer.stop()
+				self.logic(fileIDs, j, path)
+				performanceMeasures[j][i] = self.__timer.stop()
 
-		return downloadTimes
+		return performanceMeasures, fileIDs
+
+	def logic(self, fileIDs, j, path):
+		raise NotImplementedError("Must override method")
 
 
-class IPFSManager:
+class HTTPDownload(Action):
+	def __init__(self, sourceDirectory, targetDirectory):
+		super().__init__(self.__loadSourcePaths(sourceDirectory))
+		self.__downloadDirectory = targetDirectory
+		self.__fileRemover = FileRemoverOS(self.__downloadDirectory)
+
+	def __loadSourcePaths(self, sourceDirectory):
+		with open(sourceDirectory, "r") as file_in:
+			data = json.loads(file_in.read())
+			key = list(data.keys())[0]
+			return data[key]
+
+	def logic(self, fileIDs, j, url):
+		fileIDs[j] = self.__downloadDirectory + "file" + str(j+1) + ".txt"
+		response = requests.get(url, allow_redirects=True)
+		with open(fileIDs[j], "wb") as file_out:
+			file_out.write(response.content)
+
+	def download(self, runs=1):
+		return super().execute(self.__fileRemover, runs)
+
+
+class Serializer(Action):
+	def __init__(self, sourceDirectory, targetDirectory):
+		super().__init__(sourceDirectory)
+		self.__storageDirectory = targetDirectory
+		self.__fileRemover = FileRemoverOS(self.__storageDirectory)
+
+	def logic(self, fileIDs, j, path):
+		fileIDs[j] = self.__storageDirectory + "file" + str(j+1) +".json"
+		with open(path, "r", encoding="latin1") as file_in, open(fileIDs[j], "w") as file_out:
+			data = file_in.readlines()
+			json.dump(data, file_out)
+
+	def serialize(self, runs=1):
+		return super().execute(self.__fileRemover, runs)
+
+
+class IPFSUpload(Action):
+	def __init__(self, sourceDirectory, ipfsClient):
+		super().__init__(sourceDirectory)
+		self.__ipfsClient = ipfsClient
+		self.__fileRemover = FileRemoverIPFS(self.__ipfsClient)
+
+	def upload(self, runs=1):
+		return super().execute(self.__fileRemover, runs) 
+
+	def logic(self, fileIDs, j, path):
+		result = self.__ipfsClient.add(path, wrap_with_directory=True)
+		directoryHash = result[1]['Hash']
+		fileName = result[0]['Name']
+		fileIDs[j] = [directoryHash, fileName]
+
+
+class IPFSClient:
 	def __init__(self):
-		self.client = ipfshttpclient.connect()
+		self.__client = ipfshttpclient.connect()
 		self.__confirmInstance()
 
 	def __confirmInstance(self):
-		print("\nIPFS daemon running on version: " + self.client.version()["Version"])
-		print("Node IP: " + self.client.id()["Addresses"][2])
+		print("\nIPFS daemon running on version: " + self.__client.version()["Version"])
+		print("Node IP: " + self.__client.id()["Addresses"][2])
+
+	def getClient(self):
+		return self.__client
+
+
+class IPFSDistributor:
+	def __init__(self, sourceDirectory, targetIDs):
+		self.__configfilePath = sourceDirectory
+		self.__targetIDs = targetIDs
+		self.__client = paramiko.SSHClient()
+		self.__distributedCIDs = ["" for i in range(len(self.__targetIDs))]
+
+	def distribute(self):
+		ip, user, key = self.__loadIP()
+		self.__client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+		self.__client.connect(ip, 22, user, key, timeout=10)
+		
+		for i in range(len(self.__targetIDs)):
+			stdin, stdout, stderr = self.__client.exec_command("ipfs get" + " " + self.__targetIDs[i][0] + "/" + self.__targetIDs[i][1])
+			stdin, stdout, stderr = self.__client.exec_command("ipfs add -w" + " " + self.__targetIDs[i][1])
+
+			storageInfos = codecs.decode(stdout.read()).split( )
+			self.__distributedCIDs[i] = storageInfos[-1] + "/" + storageInfos[2]
+		
+		self.__client.close()
+		return self.__distributedCIDs
+		
+	def __loadIP(self):
+		with open(self.__configfilePath, "r") as file_in:
+			data = json.loads(file_in.read())
+			return data["ip"], data["user"], data["key"]
+
+
+# Auxiliary classes
+# -----------------------------------------------------------
+class FileRemoverOS:
+	def __init__(self, targetDirectory):
+		self.__targetDirectory = targetDirectory
+
+	def execute(self):
+		for file in os.scandir(self.__targetDirectory):
+				os.remove(file.path)
+
+
+class FileRemoverIPFS:
+	def __init__(self, ipfsClient):
+		self.__ipfsClient = ipfsClient
+
+	def execute(self):
+		pinnedContent = self.__ipfsClient.pin.ls(type='recursive')['Keys']
+
+		for i in list(pinnedContent.keys()):
+			self.__ipfsClient.pin.rm(i)
+		self.__ipfsClient.repo.gc()
 
 
 class Timer:
--- main.py
+++ main.py
@@ -1,69 +1,21 @@
-import paramiko
 from ipfsapp import *
 import getpass
 
 # prerequisites: a running instance of an IPFS daemon (<= 0.8.0)
 
 if __name__ == '__main__': 
-	#client = IPFSManager()
+	client = IPFSClient().getClient()
 
-	""" HTTP Download Performance
-	---------------------------------------------------------
-	1st argument: path to file containing direct link URLs
-	2nd argument: path to download directory
-	"""
-	downloadTimes = DownloadManagerHTTP("config/sourceData.json", "data/source/").downloadFiles(runs=5)
+	downloadTimes, filePathsSource = HTTPDownload("config/sourceData.json", "data/source/").download(runs=1)
 	print(downloadTimes)
+	print(filePathsSource)
 
-	"""
-	ip = "192.168.0.4"
-	port = 22
-	
-	ssh = paramiko.SSHClient()
-	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-	ssh.connect(ip, port, getpass.getpass(prompt="User: "), getpass.getpass(), timeout=10)
+	serializationTimes, filePathsSerial = Serializer(filePathsSource, "data/serialized/").serialize(runs=1)
+	print(serializationTimes)
+	print(filePathsSerial)
 
-	stdin, stdout, stderr = ssh.exec_command("systemctl status ipfs.service")
+	uploadTimes, ipfsIDs = IPFSUpload(filePathsSerial, client).upload(runs=1)
+	print(uploadTimes)
 
-	result= stdout.read()
-	print(result)
-	ssh.close()
-	
-	"""
-
-	"""
-	
-	# connecting to the local IPFS node
-	client = ipfshttpclient.connect()
-	print("\nIPFS daemon running on version: " + client.version()["Version"])
-	print("\nNode IP: " + client.id()["Addresses"][2])
-
-	# serializing the data to be stored
-	t0 = time.perf_counter()
-
-	with open("data/1mb.txt", "r", encoding="latin1") as file_in, open("data/1mb.json", "w") as file_out:
-		data = file_in.readlines()
-		json.dump(data, file_out)
-
-	t1 = time.perf_counter()
-	print(f"Time to serialize: {t1-t0:0.4f} seconds")
-
-	# adding a file to IPFS
-	retrieval_data = client.add("data/1mb.json", wrap_with_directory=True)
-	print(retrieval_data)
-
-	# read the file's content via directory hash
-	#print((client.cat(retrieval_data[1]["Hash"] + "/" + retrieval_data[0]["Name"])).decode('UTF-16'))
-
-	# download file from ipfs
-	t2 = time.perf_counter()
-	client.get(retrieval_data[1]["Hash"] + "/" + retrieval_data[0]["Name"], "tmp/")
-	t3 = time.perf_counter()
-	print(f"Time to download from local IPFS node: {t3-t2:0.04f} seconds")
-
-
-
-	"""
-
-
-
+	distributedIDs = IPFSDistributor("config/ssh.json", ipfsIDs).distribute()
+	print(distributedIDs)
